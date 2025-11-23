333c95d4696e1d09927762ec5f394d29c1162b65
CeliaClaire <119377002+seliaYYDS@users.noreply.github.com>
2025-11-23 21:41:34 +0800

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/sdapi': {
+        target: 'http://127.0.0.1:7860',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/sdapi/, '/sdapi')
+      }
+    }
+  }
+})
